5fb28090ee9d3ae7543d96e28e65d030c42a9dcf
Tal-Kuznitzki <132784614+Tal-Kuznitzki@users.noreply.github.com>
2025-10-30 17:30:00 +0200

    fixed ellipse creation

--- Functions/ellipseCreator.m
+++ Functions/ellipseCreator.m
@@ -5,15 +5,15 @@ function [radar_i_fitted, radar_q_fitted] = ellipseCreator(radar_i, radar_q,plot
         data = [radar_i_centered radar_q_centered];
         C = cov(data);
         [V, D] = eig(C);
-       % [eigvals, idx] = sort(diag(D), 'ascend');
-      %  D = diag(eigvals);
-      %  V = V(:, idx);
+        [eigvals, idx] = sort(diag(D), 'ascend');
+        D = diag(eigvals);
+        V = V(:, idx);
         %normalizing 
         D_inv_sq = diag(1 ./ sqrt(diag(D)));
         % by using A = VDV' 
         transformed_C = (D_inv_sq * V' * data')' ;
-        radar_i_fitted = transformed_C(:,1);
-        radar_q_fitted = transformed_C(:,2);
+        radar_i_fitted = -1 .* transformed_C(:,1);
+        radar_q_fitted = -1 .* transformed_C(:,2);
 
         if plotBool
             EllipsePlotter(radar_i, radar_q,radar_i_centered,radar_q_centered,radar_i_fitted,radar_q_fitted)
