7951af8e0201a0fe999ac7bc2c1c176dc3944a9d
PoritonEE <orihefetz@campus.technion.ac.il>
2025-11-15 20:44:27 +0200

    some checkups

--- Functions/ellipseCreator.m
+++ Functions/ellipseCreator.m
@@ -1,22 +1,26 @@
 function [radar_i_fitted, radar_q_fitted] = ellipseCreator(radar_i, radar_q,plotBool)
     
         radar_i_centered = radar_i - mean(radar_i);
+        radar_i_centered = radar_i_centered(:);
         radar_q_centered = radar_q - mean(radar_q);
+        radar_q_centered = radar_q_centered(:);
         data = [radar_i_centered radar_q_centered];
         C = cov(data);
         [V, D] = eig(C);
-       % [eigvals, idx] = sort(diag(D), 'ascend');
-      %  D = diag(eigvals);
-      %  V = V(:, idx);
+       [eigvals, idx] = sort(diag(D), 'ascend');
+       D = diag(eigvals);
+       V = V(:, idx);
         %normalizing 
         D_inv_sq = diag(1 ./ sqrt(diag(D)));
         % by using A = VDV' 
         transformed_C = (D_inv_sq * V' * data')' ;
-        radar_i_fitted = transformed_C(:,1);
+        radar_i_fitted = -transformed_C(:,1);
         radar_q_fitted = transformed_C(:,2);
 
         if plotBool
             EllipsePlotter(radar_i, radar_q,radar_i_centered,radar_q_centered,radar_i_fitted,radar_q_fitted)
+            
+
         end
         
 end
--- plots/EllipsePlotter.m
+++ plots/EllipsePlotter.m
@@ -5,17 +5,23 @@ function [] = EllipsePlotter(radar_i, radar_q,radar_i_centered,radar_q_centered,
         title(['radar_i vs. radar_q - regular ']);
         xlabel('radar_i');
         ylabel('radar_q');
+        axis equal
+
         grid on;
         subplot(3,1,2);
         scatter(radar_i_centered, radar_q_centered,'filled'); % 5 is the marker size
         title(['radar_i vs. radar_q - centered ']);
         xlabel('radar_i');
         ylabel('radar_q');
+        axis equal
+
         grid on;
         subplot(3,1,3);
         scatter(radar_i_fitted, radar_q_fitted,'filled'); % 5 is the marker size
         title(['radar_i vs. radar_q - after transformation ']);
         xlabel('radar_i');
         ylabel('radar_q');
+        axis equal
+
         grid on;
    end
